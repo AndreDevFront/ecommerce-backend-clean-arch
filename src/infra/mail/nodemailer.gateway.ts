0cbd4dacbaa95d2d505af391a8bbf74f634cb26b
AndreDevFront <seu-email@exemplo.com>
2025-12-17 16:10:51 -0300

    fix: temporary nou use nodemailer.

--- src/infra/mail/nodemailer.gateway.ts
+++ src/infra/mail/nodemailer.gateway.ts
@@ -1,16 +1,16 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
-import { MailGateway, SendMailProps } from 'src/domain/gateways/mail.gateway';
+// import { MailerService } from '@nestjs-modules/mailer';
+// import { Injectable } from '@nestjs/common';
+// import { MailGateway, SendMailProps } from 'src/domain/gateways/mail.gateway';
 
-@Injectable()
-export class NodemailerGateway implements MailGateway {
-  constructor(private mailerService: MailerService) {}
+// @Injectable()
+// export class NodemailerGateway implements MailGateway {
+//   constructor(private mailerService: MailerService) {}
 
-  async send({ to, subject, body }: SendMailProps): Promise<void> {
-    await this.mailerService.sendMail({
-      to,
-      subject,
-      html: body,
-    });
-  }
-}
+//   async send({ to, subject, body }: SendMailProps): Promise<void> {
+//     await this.mailerService.sendMail({
+//       to,
+//       subject,
+//       html: body,
+//     });
+//   }
+// }
