ba81e4ff7636d5ca00b36180bbd08461d660394b
AndreDevFront <seu-email@exemplo.com>
2026-01-07 15:37:56 -0300

    fix: looking for mistakes aboute decreases

--- src/application/use-cases/place-order.use-case.ts
+++ src/application/use-cases/place-order.use-case.ts
@@ -70,6 +70,16 @@ export class PlaceOrderUseCase {
         );
       }
 
+      console.log(`🔍 [DEBUG] Estoque ANTES: ${product.stock}`);
+
+      product.decreaseStock(item.quantity);
+
+      console.log(`📉 [DEBUG] Estoque DEPOIS: ${product.stock}`);
+
+      await this.productRepository.save(product);
+
+      console.log(`💾 [DEBUG] Salvo no Banco!`);
+
       orderItems.push(
         new OrderItem({
           productId: product.id!,
